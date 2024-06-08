e3e0807e90f97d0ae8d05e80f4fa712e2a4c6253
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-08 13:54:56 -0600

    write tests

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe "bulk_discounts show" do
     expect(page).to have_content("Bulk Discount ##{@bd_1.id} - Percentage Discount: 20%, Quantity Threshold: #{10}")
     expect(page).to_not have_content("Bulk Discount ##{@bd_2.id} - Percentage Discount: 50%, Quantity Threshold: #{50}")
   end
+
+  it "should have a link to update the discount" do 
+    @m_1 = Merchant.create!(name: "Hair Care")
+    @bd_1 = BulkDiscount.create!(merchant_id: @m_1.id, percentage_discount: 0.2, quantity_threshold: 10)
+    
+    visit merchant_bulk_discount_path(@m_1, @bd_1)
+
+    click_link "Edit Bulk Discount"
+
+    expect(current_path).to eq edit_merchant_bulk_discount_path(@m_1, @bd_1)
+  end
 end
