fad1ece3d13bd6ee038fccfe31f07abcab1ce628
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-08 14:31:03 -0600

    write edit tests

---
+++ spec/features/bulk_discounts/edit_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "bulk_discounts edit" do
+  before :each do
+    @m_1 = Merchant.create!(name: "Hair Care")
+    @bd_1 = BulkDiscount.create!(merchant_id: @m_1.id, percentage_discount: 0.2, quantity_threshold: 10)
+    @bd_2 = BulkDiscount.create!(merchant_id: @m_1.id, percentage_discount: 0.5, quantity_threshold: 50)
+
+    visit edit_merchant_bulk_discount_path(@m_1, @bd_1)
+  end
+  
+  it "sees a form filled in with the items attributes" do
+    expect(find_field("Percentage discount").value).to eq(@bd_1.percentage_discount)
+    expect(find_field("Quantity Threshold").value).to eq(@bd_1.quantity_threshold)
+    
+    expect(find_field("Percentage discount").value).to_not eq(@bd_2.percentage_discount)
+  end
+
+  it "can fill in form, click submit, and redirect to that discount's show page and see updated info" do
+    fill_in "Percentage discount", with: "0.45"
+    fill_in "Quantity threshold", with: "25"
+    click_button "Submit"
+  end
+  
+end
