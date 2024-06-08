dd4dbcebfe6c7f15bcebec5afc3bae1692c784c8
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-06-08 13:46:02 -0600

    pass US4

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe "bulk_discounts show" do
     @bd_2 = BulkDiscount.create!(merchant_id: @m_1.id, percentage_discount: 0.5, quantity_threshold: 50)
 
     visit merchant_bulk_discounts_path(@m_1)
-
+    
     click_link("Bulk Discount ##{@bd_1.id}")
+    
     expect(current_path).to eq merchant_bulk_discount_path(@m_1, @bd_1)
     expect(page).to have_content("Bulk Discount ##{@bd_1.id} - Percentage Discount: 20%, Quantity Threshold: #{10}")
     expect(page).to_not have_content("Bulk Discount ##{@bd_2.id} - Percentage Discount: 50%, Quantity Threshold: #{50}")
-
   end
 end
